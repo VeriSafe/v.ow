6311bddfa764f2958ed4babb75e62b39d5de6a05
Lunamonkey <lunamonkey@gmail.com>
2019-05-02 12:51:42 +0100

    Files from Build

---
+++ precache-manifest.3a6b97aa48a15df99560bf7ce1a3d681.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67224f63e83c47445304145ca101550",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3019ffd8db09080873d7",
+    "url": "/static/css/main.a9b04c00.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3019ffd8db09080873d7",
+    "url": "/static/js/main.12b802da.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cc6fdf04fbc6532dbae737c70d9ac56d",
+    "url": "/static/media/verisafe-logo-mono.cc6fdf04.svg"
+  }
+]);
